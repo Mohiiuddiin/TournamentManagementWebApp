bc87b885b3bb9ad434b2f7bb9086cc1cb82d80b2
Mohiiuddiin <45856100+Mohiiuddiin@users.noreply.github.com>
2021-04-13 21:28:33 +0600

    Creating Team Model and view for the team list

--- TournamentManagementWebApp/Controllers/HomeController.cs
+++ TournamentManagementWebApp/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TournamentManagementWebApp.DAL;
 
 namespace TournamentManagementWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        public DataContext context { get; set; }
+
+        public HomeController()
+        {
+            context = new DataContext();
+        }
         public ActionResult Index()
         {
+            
             return View();
         }
 
@@ -25,6 +33,12 @@ namespace TournamentManagementWebApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Teams()
+        {
+            var teams = context.Teams.ToList();
+            return View(teams);
         }
     }
 }
---
+++ TournamentManagementWebApp/Models/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TournamentManagementWebApp.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        [Display(Name = "Team Number")]
+        public string TeamNumber { get; set; }
+        [Display(Name = "Team Name")]
+        public string TeamName { get; set; }
+        [Display(Name = "Primary Number")]
+        public string PrimaryNumber { get; set; }
+        [Display(Name = "Secondary Number")]
+        public string SecondaryNumber { get; set; }
+        [Display(Name = "Person Name")]
+        public string PersonName { get; set; }
+        [Display(Name = "Match Slot")]
+        public string MatchSlot { get; set; }
+        [Display(Name = "Match Date & Time")]
+        public string MatchDate { get; set; }
+        [Display(Name = "WhatsApp Group")]
+        public string WhatsAppGroup { get; set; }
+    }
+}
+
